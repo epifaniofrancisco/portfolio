27fb5e4056d13201862a6d8796f093a6ba0bea2b
epifaniofrancisco <epifaniofrancisco03@gmail.com>
2024-08-13 22:11:22 +0100

    refactor: change projects section

--- src/components/sections/work.tsx
+++ src/components/sections/work.tsx
@@ -1,30 +1,30 @@
-import { PROJECTS } from '@/lib/data';
-import ProjectDetails from '@/components/data-display/project-details';
-import Tag from '@/components/data-display/tag';
-import Typography from '@/components/general/typography';
-import Container from '@/components/layout/container';
+import { PROJECTS } from "@/lib/projects-data";
+import ProjectDetails from "@/components/data-display/project-details";
+import Tag from "@/components/data-display/tag";
+import Typography from "@/components/general/typography";
+import Container from "@/components/layout/container";
 
 const WorkSection = () => {
-  return (
-    <Container id="work">
-      <div className="flex flex-col items-center gap-4">
-        <div className="self-center">
-          <Tag label="Trabalhos" />
-        </div>
-        <Typography variant="subtitle" className="max-w-xl text-center">
-          Alguns projectos que já fiz:
-        </Typography>
-      </div>
+	return (
+		<Container id="work">
+			<div className="flex flex-col items-center gap-4">
+				<div className="self-center">
+					<Tag label="Trabalhos" />
+				</div>
+				<Typography variant="subtitle" className="max-w-xl text-center">
+					Alguns projetos que já fiz:
+				</Typography>
+			</div>
 
-      {PROJECTS?.map((project, index) => (
-        <ProjectDetails
-          key={index}
-          {...project}
-          layoutType={index % 2 === 0 ? 'default' : 'reverse'}
-        />
-      ))}
-    </Container>
-  );
+			{PROJECTS?.map((project, index) => (
+				<ProjectDetails
+					key={index}
+					{...project}
+					layoutType={index % 2 === 0 ? "default" : "reverse"}
+				/>
+			))}
+		</Container>
+	);
 };
 
 export default WorkSection;
--- src/lib/projects-data.tsx
+++ src/lib/projects-data.tsx
@@ -2,39 +2,45 @@ import {
 	ProjectDetails,
 } from "@/lib/types";
 
-import ProjectFiskil from "/public/images/project-fiskil.png";
+import AngoCursos from "/public/images/projects/angocursos.png";
+import JogoDigitacao from "/public/images/projects/jogo-digitacao.png";
+import NoTeuKubico from "/public/images/projects/no-teu-kubico.png";
+import RocketNews from "/public/images/projects/rocketnews.png";
+import TempoLeitura from "/public/images/projects/tempo-de-leitura.png";
+import TransportOne from "/public/images/projects/transport-one.png";
+import VerificadorBI from "/public/images/projects/verificador-bi.png";
 
 export const PROJECTS: ProjectDetails[] = [
 	{
 		name: "Verificador de Bilhete de Identidade",
 		description:
-			"Verificador de Bilhete de Identidade é um projecto Open-Source que permite as pessoas verificarem ou consultarem os seus dados do BI online com o seu número do bilhete.",
+			"Este é um projeto Open-Source que permite as pessoas verificarem ou consultarem os seus dados do BI online com o seu número do bilhete.",
 		url: "https://verificadorbiangola.vercel.app/",
-		previewImage: ProjectWingie,
+		previewImage: VerificadorBI,
 		technologies: ["HTML5", "JavaScript", "CSS3"],
 	},
 	{
 		name: "Tempo de leitura",
 		description:
 			"Um simples contador de palavras e frases para analisar quanto tempo demorarias para ler um determinado texto.",
-		url: "https://www.wingie.com",
-		previewImage: ProjectWingie,
+		url: "#none",
+		previewImage: TempoLeitura,
 		technologies: ["HTML5", "JavaScript", "Bootstrap", "CSS3"],
 	},
 	{
 		name: "Jogo da digitação",
 		description:
-			"Este projecto é um jogo da digitação na web feito com Javascrit para verificar o quão rápido você é a digitar.",
+			"Este projeto é um jogo da digitação na web feito com Javascrit para verificar o quão rápido você é a digitar.",
 		url: "https://epifaniofrancisco.github.io/jogo-digitacao-js/",
-		previewImage: ProjectWingie,
+		previewImage: JogoDigitacao,
 		technologies: ["HTML5", "JavaScript", "Bootstrap", "CSS3"],
 	},
 	{
 		name: "No Teu Kubico",
 		description:
-			"Um simples contador de palavras e frases para analisar quanto tempo demorarias para ler um determinado texto.",
+			"Site para ajudar as pessoas a encontrar professores ao domicilio online",
 		url: "https://www.noteukubico.com/",
-		previewImage: ProjectWingie,
+		previewImage: NoTeuKubico,
 		technologies: ["HTML5", "JavaScript", "Bootstrap", "CSS3", "PHP"],
 	},
 	{
@@ -42,22 +48,22 @@ export const PROJECTS: ProjectDetails[] = [
 		description:
 			"Um simples contador de palavras e frases para analisar quanto tempo demorarias para ler um determinado texto.",
 		url: "https://angocursos.vercel.app/",
-		previewImage: ProjectWingie,
+		previewImage: AngoCursos,
 		technologies: ["HTML5", "JavaScript", "Bootstrap", "CSS3"],
 	},
 	{
 		name: "Transport One",
 		description: "Uma plataforma de aluguel de todo o tipo de transporte",
-		url: "https://www.wingie.com",
-		previewImage: ProjectWingie,
+		url: "#none",
+		previewImage: TransportOne,
 		technologies: ["HTML5", "JavaScript", "Bootstrap", "CSS3"],
 	},
 	{
 		name: "RocketNews",
 		description:
-			"Rocketnews – é uma página para a captação de leads ou newsletter.",
+			"Rocketnews é uma página para a captação de leads ou newsletter.",
 		url: "https://rocketnews.vercel.app/",
-		previewImage: ProjectWingie,
+		previewImage: RocketNews,
 		technologies: ["HTML5", "JavaScript", "Bootstrap", "CSS3", "PHP"],
 	},
 ];
