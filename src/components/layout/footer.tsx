56fe8f8ce5e44dafd4a7b924beb6ab80e28a4cde
epifaniofrancisco <epifaniofrancisco03@gmail.com>
2024-08-13 16:20:05 +0100

    refactor: change footer info

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -1,8 +1,5 @@
 import { Copyright } from 'lucide-react';
-
 import Typography from '@/components/general/typography';
-import Link from '@/components/navigation/link';
-import { EXTERNAL_LINKS } from '@/lib/data';
 
 const Footer = () => {
   return (
@@ -11,24 +8,7 @@ const Footer = () => {
         <Typography className="flex items-center" variant="body3">
           <Copyright className="mr-1 inline-block h-4 w-4" />
           {new Date().getFullYear()} |&nbsp;
-          <Link
-            noCustomization
-            externalLink
-            withUnderline
-            href={EXTERNAL_LINKS.FIGMA_FILE}
-          >
-            Designed
-          </Link>
-          &nbsp;and&nbsp;
-          <Link
-            noCustomization
-            externalLink
-            withUnderline
-            href={EXTERNAL_LINKS.GITHUB_REPO}
-          >
-            coded
-          </Link>
-          &nbsp;with ❤️️ by Sagar Shah
+          &nbsp;With ❤️️ by Epifânio Franciso
         </Typography>
       </div>
     </footer>
